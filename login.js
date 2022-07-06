133401e83224fea62c585c0ecd1dcc28f48a9d7c
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-07-05 22:36:34 -0500

    add password validation

--- login.js
+++ login.js
@@ -5,9 +5,32 @@
     let inputsList = Array.from(inputs);
 
     inputsList.forEach(function(elem) {
-        elem.addEventListener("change keyup", function(event){
+        elem.addEventListener("input", function(event){
             // validate
             let formInput = this;
+            let password = document.getElementById("password");
+            // reset
+            formInput.style.borderColor = "";
+            password.style.borderColor = ""
+            let invalidFeedback = document.querySelectorAll(".invalid-feedback");
+            invalidFeedback = Array.from(invalidFeedback);
+            invalidFeedback.forEach(function(elem){
+                elem.textContent = "";
+            })
+            // compare passwords
+            if ("password-confirmation" === formInput.getAttribute("id")){
+                setTimeout(function(){
+                    if (formInput.value !== password.value){
+                        let red = "#ff0000";
+                        formInput.style.borderColor = red;
+                        password.style.borderColor = red;
+
+                        invalidFeedback.forEach(function(elem){
+                            elem.textContent = "*Password do not match";
+                        })
+                    }
+                }, 100);
+            }
         })
     })
 
@@ -18,4 +41,4 @@
             return event.preventDefault() || event.stopPropagation();
         }
     })
-})();
+})(window, document);
